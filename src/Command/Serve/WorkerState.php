bbde1795f6a80bb460c6017d495cb0d561eb3679
Martin Bruna <mbrunas.p@gmail.com>
2026-01-30 23:16:10 +0100

    Revert "refactor(SymfonyProcessManager-976): extract WorkerConfig from WorkerState"
    This reverts commit 310cb6f189d9f7223b6a16470ebe7faed2ac73d5.

--- src/Command/Serve/WorkerState.php
+++ src/Command/Serve/WorkerState.php
@@ -8,24 +8,20 @@ use Symfony\Component\Process\Process;
 
 final class WorkerState
 {
-    public readonly int $id;
-
     public function __construct(
-        private readonly WorkerConfig $config,
+        public readonly int $id,
         private ?Process $process,
         /** @var array<int, float> */
         private array $failureTimestamps,
         private float $nextStartAt,
         private bool $stopped,
         private bool $stopSignalSent,
-    ) {
-        $this->id = $this->config->id;
-    }
+    ) {}
 
     public static function create(int $id): self
     {
         return new self(
-            new WorkerConfig($id),
+            $id,
             null,
             [],
             0.0,
